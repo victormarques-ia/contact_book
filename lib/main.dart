383b38c6ffb3ca59aad4cd33f6a0ea8a090a661f
Victor <victormarques.ia@gmail.com>
2020-07-26 11:25:11 -0300

    Added home page in project root

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:contact_book/ui/home_page.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
 }
 
