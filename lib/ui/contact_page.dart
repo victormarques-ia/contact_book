b5498f890080120fedff8aec1320f77c5edcfff2
Victor <victormarques.ia@gmail.com>
2020-07-26 11:54:05 -0300

    Create e editable contact

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,11 +1,29 @@
+import 'package:contact_book/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
   @override
   _ContactPageState createState() => _ContactPageState();
 }
 
 class _ContactPageState extends State<ContactPage> {
+  Contact _editContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.contact == null) {
+      _editContact = Contact();
+    } else {
+      _editContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
